65d40721368522376e6c3542b05cc84a2e20b2da
RusdianaNana <rusdiana30122005@gmail.com>
2026-02-27 04:25:14 +0800

    final kalkulator IPadd POSTTEST_2 folderr

--- POSTTEST_1/Form1.Designer.vb
+++ POSTTEST_1/Form1.Designer.vb
@@ -22,16 +22,99 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Label1 = New Label()
+        txtIPS = New TextBox()
+        btnTambah = New Button()
+        btnReset = New Button()
+        Label2 = New Label()
+        txtIPK = New TextBox()
+        lblPredikat = New Label()
         SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(140, 117)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(105, 25)
+        Label1.TabIndex = 0
+        Label1.Text = "IP Semester"
+        ' 
+        ' txtIPS
+        ' 
+        txtIPS.Location = New Point(313, 115)
+        txtIPS.Name = "txtIPS"
+        txtIPS.Size = New Size(232, 31)
+        txtIPS.TabIndex = 1
+        ' 
+        ' btnTambah
+        ' 
+        btnTambah.Location = New Point(433, 185)
+        btnTambah.Name = "btnTambah"
+        btnTambah.Size = New Size(112, 34)
+        btnTambah.TabIndex = 2
+        btnTambah.Text = "Tambah"
+        btnTambah.UseVisualStyleBackColor = True
+        ' 
+        ' btnReset
+        ' 
+        btnReset.Location = New Point(313, 185)
+        btnReset.Name = "btnReset"
+        btnReset.Size = New Size(112, 34)
+        btnReset.TabIndex = 3
+        btnReset.Text = "Reset"
+        btnReset.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(140, 238)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(107, 25)
+        Label2.TabIndex = 4
+        Label2.Text = "IP Kumulatif"
+        ' 
+        ' txtIPK
+        ' 
+        txtIPK.Location = New Point(313, 238)
+        txtIPK.Name = "txtIPK"
+        txtIPK.ReadOnly = True
+        txtIPK.Size = New Size(232, 31)
+        txtIPK.TabIndex = 5
+        ' 
+        ' lblPredikat
+        ' 
+        lblPredikat.AutoSize = True
+        lblPredikat.Location = New Point(313, 314)
+        lblPredikat.Name = "lblPredikat"
+        lblPredikat.Size = New Size(19, 25)
+        lblPredikat.TabIndex = 6
+        lblPredikat.Text = "-"
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(10F, 25F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(lblPredikat)
+        Controls.Add(txtIPK)
+        Controls.Add(Label2)
+        Controls.Add(btnReset)
+        Controls.Add(btnTambah)
+        Controls.Add(txtIPS)
+        Controls.Add(Label1)
         Name = "Form1"
         Text = "Kalkulator IP"
         ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtIPS As TextBox
+    Friend WithEvents btnTambah As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtIPK As TextBox
+    Friend WithEvents lblPredikat As Label
 
 End Class
--- POSTTEST_1/Form1.vb
+++ POSTTEST_1/Form1.vb
@@ -1,3 +1,40 @@
 ﻿Public Class Form1
+    Dim totalIP As Double = 0
+    Dim jumlahSemester As Integer = 0
+
+    Private Sub btnTambah_Click(sender As Object, e As EventArgs) Handles btnTambah.Click
+
+        Dim input As String = txtIPS.Text.Replace(",", ".")
+        Dim ip As Double
+
+        If Double.TryParse(input, Globalization.NumberStyles.Any, Globalization.CultureInfo.InvariantCulture, ip) Then
+            totalIP += ip
+            jumlahSemester += 1
+            Dim ipk As Double = totalIP / jumlahSemester
+            txtIPK.Text = ipk.ToString("0.00")
+
+            If ipk <= 2.75 Then
+                lblPredikat.Text = "Cukup"
+            ElseIf ipk <= 3.0 Then
+                lblPredikat.Text = "Memuaskan"
+            Else
+                lblPredikat.Text = "Sangat Memuaskan"
+            End If
+
+            txtIPS.Clear()
+            txtIPS.Focus()
+
+        Else
+            MessageBox.Show("Input yg anda masukkan harus angka!")
+        End If
+
+    End Sub
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        totalIP = 0
+        jumlahSemester = 0
+        txtIPS.Clear()
+        txtIPK.Clear()
+        lblPredikat.Text = "-"
+    End Sub
 
 End Class
