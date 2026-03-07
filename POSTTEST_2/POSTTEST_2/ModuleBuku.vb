071bdebc31f2ccfb41b9890bea69ebdf3a1f8c4c
Nana <rusdiana30122005@gmail.com>
2026-03-07 17:28:39 +0800

    Add files via upload

---
+++ POSTTEST_2/POSTTEST_2/ModuleBuku.vb
@@ -0,0 +1,15 @@
+﻿Public Module ModuleBuku
+
+    Public daftarBuku(99) As String
+    Public jumlahBuku As Integer = 0
+
+    Public Sub tambahBuku(judul As String, genre As String)
+
+        If jumlahBuku < 100 Then
+            daftarBuku(jumlahBuku) = judul & " (" & genre & ")"
+            jumlahBuku += 1
+        End If
+
+    End Sub
+
+End Module
